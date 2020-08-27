673ec4d15a0cb406403fff7667a485aad427ef97
glazebro <mikefolio@gmail.com>
2020-08-27 11:01:51 -0500

    continued course work
    click functions and learning states

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  });
+
+ const [ otherState, setOtherState ] = useState('some other value');
+
+  const switchNameHandler = () => {
+    // console.log('Was clicked');
+    // Dont do this: this.state.persons[0].name = 'Maximilian';
+    setPersonsState({
+      persons: [
+        { name: 'Maximilian', age: 28},
+        { name: 'Manu', age: 29},
+        { name: 'Stephanie', age: 27}
+      ]
+     });
+  }
+
+    return (
+      <div className="App">
+       <h1>I'm a React App.</h1>
+       <p>This is really working!</p>
+       <button onClick={switchNameHandler}>Switch Name</button>
+       <Person 
+       name={personsState.persons[0].name} 
+       age={personsState.persons[0].age} />
+       <Person 
+       name={personsState.persons[1].name} 
+       age={personsState.persons[1].age}
+       click={this.switchNameHandler}>My Hobbies: Racing</Person>
+       <Person 
+       name={personsState.persons[2].name} 
+       age={personsState.persons[2].age} />
+      </div>
+    );
+  }
+
+export default app;
