a261f52eb7d172949d46029ef9a98a16d25fc7d8
glazebro <mikefolio@gmail.com>
2020-09-11 12:48:01 -0500

    updating list and click functionality
    ~

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -13,17 +14,7 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
+
 
   nameChangedHandler = (event) => {
     this.setState({
@@ -34,6 +25,12 @@ class App extends Component {
       ]
     })
   }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
+  };
 
 togglePersonsHandler = () => {
   const doesShow = this.state.showPersons;
@@ -53,8 +50,9 @@ togglePersonsHandler = () => {
     if (this.state.showPersons){
       persons = (
         <div>
-            {this.state.persons.map(person => {
+            {this.state.persons.map((person, index) => {
               return <Person 
+              click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age} />
             })}
